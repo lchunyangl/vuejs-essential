6888807439e81117151c3a4deec2a7544653d567
lilong <lchunyangl@gmail.com>
2018-06-14 13:33:52 +0800

    注册登录完成

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+
+// 使用插件
+Vue.use(VueSweetalert2)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
+  router,
+  store,
+  components: {App},
   template: '<App/>'
 })
